84a2b0e1e7c6bf089f87f486359bc3a9d1abe4c7
Bartlomiej95 <panfilbartlomiej@gmail.com>
2022-07-01 23:17:51 +0200

    Install react-router-dom and create first route - homepage with basic structure

---
+++ src/views/HomePage.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    width: 100vw;
+    height: 100vh;
+`;
+
+
+export const HomePage = () => {
+    return(
+        <Wrapper>
+            <h1>Strona główna</h1>
+        </Wrapper>
+    )
+}
