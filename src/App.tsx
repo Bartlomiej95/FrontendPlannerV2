425a5a23c388b8a98d39564618a931a981d9a68d
Bartlomiej95 <panfilbartlomiej@gmail.com>
2022-07-02 17:50:43 +0200

    Added mainTheme to project - basic styles for basic components such Button, Heading etc

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,20 @@ import React from 'react';
 import './App.css';
 import GlobalStyle from './theme/GlobalStyle';
 import { Routes, Route, Link } from "react-router-dom";
+import { ThemeProvider } from 'styled-components';
 import { HomePage } from './views/HomePage';
+import {theme} from "./theme/mainTheme";
 
 function App() {
 
   return (
     <>
       <GlobalStyle />
+      <ThemeProvider theme={theme}>
         <Routes>
           <Route path="/" element={<HomePage/>}/>
         </Routes>
+      </ThemeProvider>
     </>
   );
 }
---
+++ src/theme/mainTheme.ts
@@ -0,0 +1,36 @@
+export const theme = {
+
+    // ** Buttons
+    Button: {
+        mobileWidth: '154px',
+        mobileHeight: '40px',
+        borderRadius: '20px',
+
+        primaryBcgColor: '#0903B0',
+        primaryHoverBcgColor: '#372FFF',
+        primaryColor: 'white',
+
+        secondaryBcgColor: 'white',
+        secondaryHoverBcgColor: '#372FFF',
+        secondaryColor: '#372FFF',
+        secondaryHoverColor: 'white',
+    },
+
+    // Buttons **
+
+
+    // ** Headings
+    Heading: {
+        mobileFontSize: '28px',
+        laptopFontSize: '30px',
+        desktopFontSize: '32px',
+    },
+
+    SubHeading: {
+        mobileFontSize: '18px',
+        laptopFontSize: '20px',
+        desktopFontSize: '22px',
+    }
+
+    // Headings **
+}
