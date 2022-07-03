75b15aee8467a8a6b168f993021f1e8213099308
Bartlomiej95 <panfilbartlomiej@gmail.com>
2022-07-03 15:45:37 +0200

    Added Input component

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Input = styled.input`
+    display: block;
+    width: ${({ theme }) => theme.Input.mobileWidth};
+    height: ${({ theme }) => theme.Input.mobileHeight};
+    border: 1px solid #0903B0;
+    border-radius: ${({ theme }) => theme.Input.borderRadius};
+    padding: ${({ theme }) => theme.Input.mobilePadding};
+    margin: 10px auto;
+    outline: none;
+    
+    ::placeholder{
+        padding-left: 25px;
+        color: rgb(55, 47, 255, 0.5);
+    }
+`;
