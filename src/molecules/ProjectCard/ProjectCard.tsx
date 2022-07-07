6126955491c22b30591a9787245911a4d7de08b2
Bartlomiej95 <panfilbartlomiej@gmail.com>
2022-07-07 19:26:54 +0200

    Updated of the MainSection with the expansion of the Projects tab

---
+++ src/molecules/ProjectCard/ProjectCard.tsx
@@ -0,0 +1,90 @@
+import styled from "styled-components";
+import { SubSubHeading } from "../../components/Heading/Heading";
+import { Paragraph } from "../../components/Paragraph/Paragraph";
+
+
+const Wrapper = styled.div`
+    position: relative;
+    width: 305px;
+    min-height: 200px;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    margin: 0 auto;
+    box-shadow: 0px 2px 8px 2px rgba(0, 0, 0, 0.16);
+    margin-bottom: 25px;
+    border-radius: 10px;
+    padding: 20px;
+    background-color: white;
+`;
+
+const ProjectCardParagraph = styled(Paragraph)`
+    margin: 15px 0;
+`;
+
+const WrapperProjectCompletePercent = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    align-items: center;
+    width: 70%;
+`;
+
+const Diagram = styled.div`
+    position: relative;
+    width: 47px;
+    height: 47px;
+    border-radius: 50%;
+    border: 2px solid #0903B0;
+    margin-right: 15px;
+`;
+
+const DiagramPercent= styled(Paragraph)`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: #0903B0;
+    font-weight: 700;
+`;
+
+const WrapperLabels = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    margin-top: 20px;
+`;
+
+const LabelCard = styled.p`
+    position: absolute;
+    top: 8%;
+    right: 8%;
+    font-size: 12px;
+    color: #0903B0;
+    font-weight: 700;
+`;
+
+interface Props {
+    title: string,
+    description: string,
+}
+
+export const ProjectCard = ({ title, description }: Props) => {
+    return(
+        <Wrapper>
+            <SubSubHeading>{title}</SubSubHeading>
+            <ProjectCardParagraph>{description}</ProjectCardParagraph>
+            <WrapperProjectCompletePercent>
+                <Diagram>
+                    <DiagramPercent>100%</DiagramPercent>
+                </Diagram>
+                <Paragraph>Stopień ukończenia projektu</Paragraph>
+            </WrapperProjectCompletePercent>
+            <WrapperLabels>
+                        <p>Lorem ipsum</p>
+            </WrapperLabels>
+            <LabelCard>Zespół</LabelCard>
+        </Wrapper>
+    )
+}
--- src/organisms/MainSection/MainSection.tsx
+++ src/organisms/MainSection/MainSection.tsx
@@ -1,15 +1,23 @@
-import { useState } from "react";
-import { useSelector } from "react-redux";
+import {useEffect, useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
 import styled from "styled-components";
-import { SubHeading } from "../../components/Heading/Heading";
-import { Paragraph } from "../../components/Paragraph/Paragraph";
+import {SubHeading} from "../../components/Heading/Heading";
+import {Paragraph} from "../../components/Paragraph/Paragraph";
 import {MainSectionType} from "../../utils/enums/main-section";
 import {InnerMainSectionNav} from "../../molecules/InnerMainSectionNav/InnerMainSectionNav";
+import {getProjectsForLoggedUser} from "../../store/Projects/actions";
+import {ProjectItem} from "../../../../planner/src/project/project.entity";
+import {ProjectCard} from "../../molecules/ProjectCard/ProjectCard";
 
 const Wrapper = styled.main`
     min-height: 100vh;
     background-color: #F9FAFF;
     z-index: -2; 
+`;
+
+const WrapperProjectCard = styled.div`
+    width: 100%;
+    margin-top: 42px;
 `;
 
 const WrapperHelpdeskInfo = styled.div`
@@ -37,12 +45,18 @@ const SubHeadingHelpdesk = styled(SubHeading)`
     color: black;
 `;
 
-
 export const MainSection = () => {
 
     const auth = useSelector((state: any) => state.auth);
     const [typeOfMainSection, setTypeOfMainSection] = useState(MainSectionType.Project);
+    const dispatch = useDispatch();
+    const projects = useSelector((state: any) => state.projects.projects);
 
+    useEffect(() => {
+        if(typeOfMainSection === MainSectionType.ProjectManager){
+            dispatch(getProjectsForLoggedUser(auth.id));
+        }
+    }, [typeOfMainSection])
 
     return(
         <Wrapper>
@@ -51,6 +65,20 @@ export const MainSection = () => {
                 valueOfType={typeOfMainSection}
                 userRole={auth.role}
             />
+            {
+                typeOfMainSection === MainSectionType.Project && (
+                    <WrapperProjectCard>
+                        {
+                            projects.map( (project: ProjectItem) =>
+                                <ProjectCard
+                                    title={project.title}
+                                    description={project.description}
+                                />
+                            )
+                        }
+                    </WrapperProjectCard>
+                )
+            }
             <WrapperHelpdeskInfo>
                 <SubHeadingHelpdesk >Masz problem z obsługą planera?</SubHeadingHelpdesk>
                 <ParagraphHelpdesk > <SpanHelpdesk>Zgłoś się</SpanHelpdesk> do naszego helpdesku</ParagraphHelpdesk>
